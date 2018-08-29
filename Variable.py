a60f988eebc921d951ad9ef9acfe054ac62578e8
772766964 <42690273+772766964@users.noreply.github.com>
2018-08-29 23:48:05 +0800

    Update Variable.py

--- Variable.py
+++ Variable.py
@@ -1,7 +1,10 @@
+# python会根据赋予的类型来决定该变量的类型
+# char(),str(),int()...可以进行强制转型
+
 counter = 100 # 赋值整型变量
 miles = 1000.0 # 浮点型
 name = "John" # 字符串
  
-print counter
+print counter  
 print miles
 print name
